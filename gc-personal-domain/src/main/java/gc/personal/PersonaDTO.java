f1c4f0c9aa5b7678b0cbd70420b5abb7b3816fc9
Fede <fartjor@gobiernodecanarias.org>
2019-11-28 15:01:15 +0100

    Add intial project

---
+++ gc-personal-domain/src/main/java/gc/personal/PersonaDTO.java
@@ -0,0 +1,13 @@
+package gc.personal;
+
+public class PersonaDTO {
+    private Integer id;
+    private String name;
+    
+    public PersonaDTO(){
+        this.id = id;
+        this.name = name;
+    }
+    
+    
+}
